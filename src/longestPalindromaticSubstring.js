df9eec95e55f771fd5a8a39666c3ec515f35ed9b
kyusulamita <sulamitamorales@yahoo.com>
2018-10-17 01:04:41 -0400

    forgot to do

--- src/longestPalindromaticSubstring.js
+++ src/longestPalindromaticSubstring.js
@@ -1,13 +1,16 @@
 const palindromeHelper = (str, i, isEvenStr) => {
   let front = i;
   let back = i + isEvenStr;
-  while (front >= 0 && back <= str.length){
-    if (str[front--] !== str[back++]){
-      front+=2;
-      back-= 2;
+  console.log('HELLO, hi', front, back);
+
+  while (front > 0 && back < str.length -1){
+    if (str[front - 1] !== str[back + 1]){
       break;
     }
+    front--;
+    back++;
   }
+  console.log('HELLO, hi2', front, back, str.slice(front, back));
   return str.slice(front, back);
 }
 
@@ -25,5 +28,6 @@ export const longestPalindromaticSubstring = (str) => {
 
     longest = longest.length < longestAtI.length ? longestAtI : longest;
   }
-  return longest;
+  console.log(longest)
+  return longest.length > 1 ? longest : "";
 };
